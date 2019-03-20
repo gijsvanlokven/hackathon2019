511b25155b42636321fcf9cad56cbd39b821f066
Robin Roosenbrand <robin.roosenbrand@edu.kw1c.nl>
2019-03-20 13:26:06 +0100

    added & completed partial header

---
+++ public/pages/contact.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html class="no-js" lang="en">
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Learn2Gether</title>
+    <link rel="stylesheet" href="../foundation6.5.1/css/foundation.css" />
+    <link rel="stylesheet" href="../stylesheets/contact.css" />
+</head>
+<body>
+    <script src="../foundation6.5.1/js/vendor/jquery.js"></script>
+    <script src="../foundation6.5.1/js/vendor/what-input.js"></script>
+    <script src="../foundation6.5.1/js/vendor/foundation.min.js"></script>
+    <script src="../script/randomize.js" defer></script>
+    <link rel="import" href="../partials/header.html">
+</body>
+    <?php
+        include '../partials/header.html';
+    ?>
+</html>
