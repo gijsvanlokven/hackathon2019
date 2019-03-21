0196070d385109be5a8b1215384d1f3de3cbb66d
Robin Roosenbrand <robin.roosenbrand@edu.kw1c.nl>
2019-03-21 14:53:41 +0100

    added courses page to site

---
+++ public/pages/courses.php
@@ -0,0 +1,159 @@
+<!doctype html>
+<html class="no-js" lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta http-equiv="x-ua-compatible" content="ie=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>Learn2Gether</title>
+        <link rel="stylesheet" href="../foundation6.5.1/css/foundation.css" />
+        <link rel="stylesheet" href="../stylesheets/main.css" />
+        <link rel="stylesheet" href="../stylesheets/courses.css" />
+    </head>
+    <body>
+        <script src="../foundation6.5.1/js/vendor/jquery.js"></script>
+        <script src="../foundation6.5.1/js/vendor/what-input.js"></script>
+        <script src="../foundation6.5.1/js/vendor/foundation.min.js"></script>
+        <script src="../script/randomize.js" defer></script>
+        <script src="../script/parralax.js" defer></script>
+        <script>
+            $(document).foundation();
+        </script>
+        <link rel="stylesheet" href="../stylesheets/headerStylePartials.css" />
+        <header>
+            <?php include "../partials/header.html" ?>
+        </header>
+        <section id='contentSection'>
+            <section id="coursesContainer">
+                <div class="grid-container full" id="coursesConfigBar">
+                    <div class="grid-x grid-margin-x small-up-1 medium-up-2 large-up-4" >
+                        <div class="cell"></div>
+                        <div class="cell"></div>
+                        <div class="cell">
+                            <div class="clickContainer">
+                                <img src="../img/plus.svg"> 
+                                Create a course
+                            </div>
+                        </div>
+                        <div class="cell">
+                            <div class="clickContainer">
+                                <img src="../img/question.svg"> 
+                                Suggest a course
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <h3>
+                    Courses
+                </h3>
+                <div id="coursesWrapper" class="grid-container full">
+                    <div class="grid-x grid-margin-x small-up-1 medium-up-2 large-up-6" >
+                        <div class="cell">
+                            <div class="grid-y">
+                                <div class="cell large-12 medium-12 small-12">
+                                    <img src="../img/hackaton_logo.png" alt="">
+                                </div>
+                                <div class="cell large-12 medium-12 small-12">
+                                    <h6>HTML</h6>
+                                </div>
+                                <div class="cell large-12 medium-12 small-12">
+                                    <p>asdas</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="cell">
+                            <div class="grid-y">
+                                <div class="cell large-12 medium-12 small-12">
+                                    <img src="../img/hackaton_logo.png" alt="">
+                                </div>
+                                <div class="cell large-12 medium-12 small-12">
+                                    <h6>HTML</h6>
+                                </div>
+                                <div class="cell large-12 medium-12 small-12">
+                                    <p>asdas</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="cell">
+                            <div class="grid-y">
+                                <div class="cell large-12 medium-12 small-12">
+                                    <img src="../img/hackaton_logo.png" alt="">
+                                </div>
+                                <div class="cell large-12 medium-12 small-12">
+                                    <h6>HTML</h6>
+                                </div>
+                                <div class="cell large-12 medium-12 small-12">
+                                    <p>asdas</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="cell">
+                            <div class="grid-y">
+                                <div class="cell large-12 medium-12 small-12">
+                                    <img src="../img/hackaton_logo.png" alt="">
+                                </div>
+                                <div class="cell large-12 medium-12 small-12">
+                                    <h6>HTML</h6>
+                                </div>
+                                <div class="cell large-12 medium-12 small-12">
+                                    <p>asdas</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="cell">
+                            <div class="grid-y">
+                                <div class="cell large-12 medium-12 small-12">
+                                    <img src="../img/hackaton_logo.png" alt="">
+                                </div>
+                                <div class="cell large-12 medium-12 small-12">
+                                    <h6>HTML</h6>
+                                </div>
+                                <div class="cell large-12 medium-12 small-12">
+                                    <p>asdas</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="cell">
+                            <div class="grid-y">
+                                <div class="cell large-12 medium-12 small-12">
+                                    <img src="../img/hackaton_logo.png" alt="">
+                                </div>
+                                <div class="cell large-12 medium-12 small-12">
+                                    <h6>HTML</h6>
+                                </div>
+                                <div class="cell large-12 medium-12 small-12">
+                                    <p>asdas</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="cell">
+                            <div class="grid-y">
+                                <div class="cell large-12 medium-12 small-12">
+                                    <img src="../img/hackaton_logo.png" alt="">
+                                </div>
+                                <div class="cell large-12 medium-12 small-12">
+                                    <h6>HTML</h6>
+                                </div>
+                                <div class="cell large-12 medium-12 small-12">
+                                    <p>asdas</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="cell">
+                            <div class="grid-y">
+                                <div class="cell large-12 medium-12 small-12">
+                                    <img src="../img/hackaton_logo.png" alt="">
+                                </div>
+                                <div class="cell large-12 medium-12 small-12">
+                                    <h6>HTML</h6>
+                                </div>
+                                <div class="cell large-12 medium-12 small-12">
+                                    <p>asdas</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </section>
+    </body>
+</html>
