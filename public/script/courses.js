3160dcbb136040ce8f37fa63341718f36b00ed4a
Frank Bekema <frank.bekema@edu-kw1c.nl>
2019-03-22 09:16:54 +0100

    Finished Course API

---
+++ public/script/courses.js
@@ -0,0 +1,26 @@
+
+fetch("http://localhost:8080/api/courses")
+    .then(function(response) {
+        return response.json();
+    })
+    .then(function(responseJson) {
+        responseJson.forEach((json) => {
+            console.log(json)
+            createCourse(json.CourseID, json.Name, json.Description)
+        });
+    });
+
+function createCourse(courseId, name, description = "")
+{
+    let courseHTML = `
+    <div class="cell clickContainer course" id="${courseId}" onclick='window.location.href="question.html?course=${courseId}"'">
+        <div class="grid-y">
+            <div class="cell large-12 medium-12 small-12">
+                <h6>${name}</h6>
+            </div>
+            <div class="cell large-12 medium-12 small-12">
+                <p>${description}</p>
+            </div>
+        </div>`
+    document.querySelector("#coursesWrapper > div").insertAdjacentHTML('beforeend', courseHTML);
+}
