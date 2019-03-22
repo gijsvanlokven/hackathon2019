6c2a993861a5c1511acffbb1c7686c19eb5aa256
roy-ermers <dangergamesnl@gmail.com>
2019-03-22 11:17:35 +0100

    fixed courses error

--- public/script/courses.js
+++ public/script/courses.js
@@ -1,19 +1,17 @@
-
 fetch("https://www.energylog.nl/api/courses")
-    .then(function(response) {
+    .then(function (response) {
         return response.json();
     })
-    .then(function(responseJson) {
+    .then(function (responseJson) {
         responseJson.forEach((json) => {
             console.log(json)
             createCourse(json.CourseID, json.Name, json.Description)
         });
     });
 
-function createCourse(courseId, name, description = "")
-{
+function createCourse(courseId, name, description = "") {
     let courseHTML = `
-    <div class="cell clickContainer course" id="${courseId}" onclick='window.location.href="question.html?course=${courseId}"'">
+    <div class="cell clickContainer course" id="${courseId}" onclick='window.location.href="question.php?course=${courseId}"'">
         <div class="grid-y">
             <div class="cell large-12 medium-12 small-12">
                 <h6>${name}</h6>
