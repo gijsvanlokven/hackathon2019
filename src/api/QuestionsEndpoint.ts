f4116405694ae9f2624d16a4bcbffa3ad3e75da6
Frank Bekema <frank.bekema@edu-kw1c.nl>
2019-03-24 20:01:12 +0100

    Fix

--- src/api/QuestionsEndpoint.ts
+++ src/api/QuestionsEndpoint.ts
@@ -95,7 +95,7 @@ export default class QuestionsEndpoint implements APIEndpoint {
 		else res.sendStatus(400);
 	}
 	async MaxID(req, res) {
-		let result = await database.query("SELECT MAX(QuestionID) as LastID FROM Question");
+		let result = await database.query("SELECT AUTO_INCREMENT FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_SCHEMA = 'Hackathon' AND TABLE_NAME  = 'Question'");
 		res.send(result.results[0]);
 	}
 }
